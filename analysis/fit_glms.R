e6e890bf950da3b8beb039f55d1ab69c1bc48172
David Clarke <david_anthony_clarke@hotmail.com>
2025-03-31 10:37:46 +1100

    Create fit_glms.R

---
+++ analysis/fit_glms.R
@@ -0,0 +1,37 @@
+#-------------------------------------------------------------------------------
+# Load covariate data and response data
+#-------------------------------------------------------------------------------
+
+## Define file path
+outpath <- here("data", "output")
+
+## load true covariate
+load(here(outpath, "covar_dat_full.rdata"))
+
+## load downscaled covariate 
+xx1_pred <- loadRData(here(outpath, "sim_all_x1_gpbau_pred.RData"))
+xx2_pred <- loadRData(here(outpath, "sim_all_x2_gpbau_pred.RData"))
+xx3_pred <- loadRData(here(outpath, "sim_all_x3_gpbau_pred.RData"))
+
+## load response data
+pois_rep <- loadRData(here(outpath, "pois_rep_data.rdata"))
+nrep <- length(pois_rep)
+
+## combine covariate data into lists
+fine_list <- list(fine_xx_1, fine_xx_2, fine_xx_3)
+xx_pred <- list(xx1_pred, xx2_pred, xx3_pred)
+
+## Split training and testing data
+fine_train_zz <- pois_rep[[1]]$train$fine_train_zz
+fine_train_xx <- pois_rep[[1]]$train$fine_train_xx
+
+#-------------------------------------------------------------------------------
+# Fit GLMs
+#-------------------------------------------------------------------------------
+fitted_glms <- fitglms(zz = fine_train_zz, 
+                       xx = fine_list, 
+                       xx_ppd = xx_pred, 
+                       BB = 4, 
+                       nphi = 1, 
+                       nsill = 1)
+
