1cbb69e75c61197c3de537c0eb8ce8eadcb340b4
David Clarke <david_anthony_clarke@hotmail.com>
2025-04-03 10:46:11 +1100

    Add rlimpois function
    Simulate Poisson data within specified bounds.

--- R/utils.R
+++ R/utils.R
@@ -6,3 +6,13 @@ loadRData <- function(fileName){
   get(ls()[ls() != "fileName"])
   
 }
+
+## Simulate Poisson data within bounds
+rlimpois <- function(n, lambda, lowlimit, toplimit){
+  
+  sample(x = lowlimit:toplimit, 
+         size = n,
+         prob = dpois(lowlimit:toplimit, lambda), 
+         replace = TRUE)
+  
+}
