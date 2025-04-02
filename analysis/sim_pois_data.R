93a0358da47782418a01e34d71633af7c4cd5167
David Clarke <david_anthony_clarke@hotmail.com>
2025-04-02 15:17:47 +1100

    Update sim_pois_data.R

--- analysis/sim_pois_data.R
+++ analysis/sim_pois_data.R
@@ -19,8 +19,8 @@ nsill <- dim(fine_xx_1)[3]
 #-------------------------------------------------------------------------------
 b0 <- 1
 b1 <- 0.5
-b2 <- 2
-b3 <- 3
+b2 <- 0.5
+b3 <- 0.5
 true_be <- c(b0, b1, b2, b3) # Values don't matter at this point.
 nn <- nrow(fine_xx_1)
 nrep <- 100
@@ -73,4 +73,4 @@ for (r in 1:nrep) {
   
 }
 
-save(rep_data, file = here(outpath, "pois_rep_data.rdata"))
+save(rep_data, file = here(outpath, "pois_rep_data_all.rdata"))
